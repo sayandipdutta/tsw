5e8a6e42a49af8a527118fe49d84c2b19b31ae99
sayandipdutta <sayandip199309@gmail.com>
2024-08-29 23:54:35 +0530

    refactor: make apply_theme totally iterator based

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     let config = parse_config().expect("Config not found!");
     let args = Args::parse();
     let theme_to_set = args.theme.unwrap_or_else(get_win_theme);
-    apply_theme(theme_to_set, config);
+    apply_theme(theme_to_set.into(), config);
 }
--- src/theme_manager.rs
+++ src/theme_manager.rs
@@ -1,14 +1,13 @@
 use std::fs;
-use std::{
-    borrow::Borrow, fs::hard_link, io::Error, os::unix::fs::symlink, path::Path, sync::Arc, thread,
-};
+use std::ops::Deref;
+use std::path::Path;
+use std::sync::Arc;
+use std::thread;
 
 use serde::Deserialize;
 
-use crate::{
-    config::{AppConfig, Config},
-    theme::Theme,
-};
+use crate::config::Config;
+use crate::theme::Theme;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
@@ -17,52 +16,43 @@ pub enum LinkMethod {
     Hard,
 }
 
-pub fn apply_theme(theme: Theme, config: Config) {
-    let mut handles = vec![];
-    let theme = Arc::new(theme);
-    for (app_name, app_config) in config.apps {
-        let app_config = Arc::new(app_config);
-        let app_name = Arc::new(app_name);
-        let theme = Arc::clone(&theme);
-        let handle = thread::spawn(move || {
-            let c = app_config.borrow();
-            let t = theme.borrow();
-            let file = get_filename_based_on_theme(c, &t);
-            if !Path::new(file).is_file() {
-                println!("Config for {} not found", app_name);
-                return;
-            }
-            let status = link(file, &c.target, &c.method);
-            if let Err(e) = status {
-                println!(
-                    "Error occurred while linking config for {}: {:?}",
-                    app_name, e
-                );
-            }
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        handle.join().unwrap();
-    }
+pub fn apply_theme(theme: Arc<Theme>, config: Config) {
+    config
+        .apps
+        .into_iter()
+        .map(|(app_name, app_config)| {
+            let app_name = Arc::new(app_name);
+            let app_config = Arc::new(app_config);
+            let theme = Arc::clone(&theme);
+
+            thread::spawn(move || {
+                let file = match theme.deref() {
+                    Theme::Light => &app_config.light,
+                    Theme::Dark => &app_config.dark,
+                };
+                if !Path::new(file).is_file() {
+                    eprintln!("Config for {} not found", app_name);
+                    return;
+                }
+
+                if let Err(e) = link(file, &app_config.target, &app_config.method) {
+                    eprintln!(
+                        "Error occurred while linking config for {}: {:?}",
+                        app_name, e
+                    );
+                }
+            })
+        })
+        .for_each(|handle| handle.join().unwrap());
 }
 
-fn link(source: &str, target: &str, method: &LinkMethod) -> Result<(), Error> {
-    if Path::new(&target).is_file() | Path::new(&target).is_symlink() {
-        fs::remove_file(&target)?;
-    } else {
-        eprintln!("Target file exists, and not a file or symlink {}", &target);
-        return Ok(());
+fn link(source: &str, target: &str, method: &LinkMethod) -> Result<(), std::io::Error> {
+    if Path::new(target).exists() {
+        fs::remove_file(target)?;
     }
+
     match method {
-        LinkMethod::Hard => hard_link(&source, &target),
-        LinkMethod::Soft => symlink(&source, &target),
-    }
-}
-
-fn get_filename_based_on_theme<'a>(config: &'a AppConfig, theme: &Theme) -> &'a str {
-    match theme {
-        Theme::Light => &config.light,
-        Theme::Dark => &config.dark,
+        LinkMethod::Hard => fs::hard_link(source, target),
+        LinkMethod::Soft => std::os::unix::fs::symlink(source, target),
     }
 }
